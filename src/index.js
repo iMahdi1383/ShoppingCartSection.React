e339aadfcf08722886ffaa2fd58923d08f7c4db2
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-07-01 22:58:05 +0430

    Init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+return <div className="container" id="title">
+    <h1>Shopping App</h1>
+    <p>product #1 : book 1</p>
+</div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App/>, document.querySelector('#root'));
