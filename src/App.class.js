7ab64ba022823f07405887975651c29aaf04e037
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-08-17 17:42:45 +0430

    App.class: use bind() instead of callback function

--- src/App.class.js
+++ src/App.class.js
@@ -1,17 +1,32 @@
 import { Component } from 'react';
-import './App.css'
+import './App.css';
 import Product from './Components/Product/Product';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    /*
+    ? How does bind() work? :
+    #1- Copy this.RefreshPrices()
+    #2- Replace 'this' keyword in the copied function.
+    #3- returns new function with 'this'
+    then assign (=) it to old function.
+    * Also we can use this in class, instead of using 'bind()' method in constructor :
+    * RefreshPrices = () => {}
+    */
+    this.RefreshPrices = this.RefreshPrices.bind(this);
+  }
   state = {
     products: [
-      { id:'1', name: 'Udemy JS', price: '350' },
-      { id:'2', name: 'Udemy HTML', price: '270' },
-      { id:'3', name: 'Udemy React', price: '890' },
+      { id: '1', name: 'Udemy JS', price: '350' },
+      { id: '2', name: 'Udemy HTML', price: '270' },
+      { id: '3', name: 'Udemy React', price: '890' },
     ],
   };
 
-  RefreshPrices = () => {
+  RefreshPrices() {
+    console.log('App.RefreshPrices().this : ', this);
+    /*
     this.setState(
       (prevState) => {
         return ({
@@ -25,27 +40,23 @@ class App extends Component {
         });
       }
     );
-    /*
-    this.setState({
-      products: [
-        { name: 'Udemy JS', price: Math.floor(Math.random() * 1000) },
-        { name: 'Udemy HTML', price: Math.floor(Math.random() * 1000) },
-        { name: 'Udemy React', price: Math.floor(Math.random() * 1000) },
-      ],
-    });
     */
-  };
+  }
 
   render() {
     return (
       <div className="container">
-      <h1> Courses Shop </h1>
+        <h1> Courses Shop </h1>
         <hr />
         {
-        /* Show Products */
+          /* Show Products */
           this.state.products.map((product) => {
             return (
-              <Product name={product.name} price={product.price + ',000 T'} key={product.id}/>
+              <Product
+                name={product.name}
+                price={product.price + ',000 T'}
+                key={product.id}
+              />
             );
           })
         }
