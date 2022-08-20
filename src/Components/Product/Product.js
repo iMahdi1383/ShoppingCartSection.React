04bdc6e1718f22742662e6cfb66128d7a4b4335c
Mahdi Askarpoor <mahdiaskarpoor1383@gmail.com>
2022-08-20 13:13:19 +0430

    'Product.js' :Use Fragment

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,12 +1,17 @@
-import Styles from './Product.module.css';
+import { Fragment } from 'react';
 
 const Product = (props) => {
   return (
-    <div className={Styles.product} onClick={props.click}>
+    /*
+    ? Fragment :
+    A JSX tag that shows content without a parrent <div> element.
+    * We Can Use <></> instead of <Fragment></Fragment>
+    */
+    <Fragment>
       <h3>Product Name : {props.name}</h3>
       <p>Product price : {props.price}</p>
       {props.children}
-    </div>
+    </Fragment>
   );
 };
 
